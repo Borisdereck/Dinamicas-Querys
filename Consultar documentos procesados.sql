14c15888873c6a8863d0aea98ce7ae9100868ba9
Boris Terraza G <ingterraza@gmail.com>
2018-02-06 17:47:38 -0500

    Archivo de documentos procesados

---
+++ Consultar documentos procesados.sql
@@ -0,0 +1,32 @@
+﻿SELECT * FROM opav.acciones where id_solicitud = '922634'
+
+
+--Paso 3
+SELECT * 
+FROM CON.MC_SL_FAC_CC_SEL 
+WHERE MC_____CODIGO____TD_____B = 'CXPN' 
+  AND MC_____CODIGO____CONTAB_B = 'SELE' 
+  AND MC_____CODIGO____CD_____B = 'CFSL'; 
+
+--Paso 4
+SELECT * 
+FROM CON.MC_SL_FAC_CC_SEL 
+WHERE MC_____CODIGO____TD_____B = 'CXPN' 
+AND MC_____CODIGO____CONTAB_B = 'SELE' 
+AND MC_____CODIGO____CD_____B = 'NCCF'; 
+
+--Paso 5
+SELECT * 
+FROM CON.MC_SL_FAC_CC_SEL 
+WHERE MC_____CODIGO____TD_____B = 'CXPN' 
+AND MC_____CODIGO____CONTAB_B = 'SELE' 
+AND MC_____CODIGO____CD_____B = 'NCTR'; 
+
+--Paso 6
+SELECT * 
+FROM CON.MC_SL_FAC_CC_SEL 
+WHERE MC_____CODIGO____TD_____B = 'CXPN' 
+AND MC_____CODIGO____CONTAB_B = 'SELE' 
+AND MC_____CODIGO____CD_____B = 'CPFI'; 
+
+
